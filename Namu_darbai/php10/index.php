90d2bf0409464ae12fd6d69d9d1632c22f8fcd23
Gabija Trimbel <gabija.trimbel@gmail.com>
2018-12-02 19:23:25 +0200

    prideti namu darbai php10

---
+++ Namu_darbai/php10/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    function plotas($num1, $num2) {
+      $area = $num1 * $num2;
+      echo "Stačiakampio plotis: $area";
+    }
+
+    plotas(5, 3);
+    ?>
+  </body>
+</html>
